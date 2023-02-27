cd832e4c56f6936034aa3ba45963300cf3b56422
Yelena Khvan <117268469+YelenaKhvan@users.noreply.github.com>
2023-02-27 16:03:25 +0600

    feat: test

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.guessNumber = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessNumber = Math.floor((this.min + this.max) / 2);
+        return this.guessNumber;
     }
 
     lower() {
-
+        this.max = this.guessNumber;
     }
 
     greater() {
-
+        this.min = this.guessNumber;
     }
 }
 
 module.exports = GuessingGame;
+
+
